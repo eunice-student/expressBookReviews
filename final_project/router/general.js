acea6245a243efebd9e249bd97fa7227a6de155f
eunice-student <eunicevr71@gmail.com>
2026-08-24 15:17:01 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,8 +20,13 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    res.send(JSON.stringify(books[isbn], null, 2));
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 
